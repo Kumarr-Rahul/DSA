3e88ed64bfa88a17279a0b9c504fc1592ba1f6ef
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-08-02 19:57:07 +0530

    Time: 346 ms (7.29%), Space: 149.8 MB (55.87%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,26 +1,69 @@
+// class Solution {
+//     public int[] maxSlidingWindow(int[] nums, int k) {
+//         int[]res = new int[nums.length - k + 1];
+//         int idx = 0;
+        
+//         Deque<Integer> q = new ArrayDeque<>();
+//         for(int i = 0; i < nums.length; i++) {
+            
+//             if(q.size() > 0 && q.getFirst() <= i - k) {
+//                 q.removeFirst();
+//             }
+            
+//             while(q.size() > 0 && nums[q.getLast()] < nums[i]) {
+//                 q.removeLast();
+//             }
+            
+//             q.addLast(i);
+            
+//             if(i >= k - 1){
+//                 res[idx++] = nums[q.getFirst()];
+//            }
+//         }
+        
+//         return res;
+//     }
+// }
+
+//2nd Approach
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[]res = new int[nums.length - k + 1];
-        int idx = 0;
+        int[] nge = new int[nums.length];
+        Stack<Integer> st = new Stack<>();
         
-        Deque<Integer> q = new ArrayDeque<>();
-        for(int i = 0; i < nums.length; i++) {
-            
-            if(q.size() > 0 && q.getFirst() <= i - k) {
-                q.removeFirst();
+        st.push(nums.length - 1);
+        nge[nums.length - 1] = nums.length;
+        
+        for(int i = nums.length - 2; i >= 0; i--){
+            int val = nums[i];
+            while(st.size() > 0 && val >= nums[st.peek()]){
+                st.pop();
             }
             
-            while(q.size() > 0 && nums[q.getLast()] < nums[i]) {
-                q.removeLast();
+            if(st.size() == 0){
+                nge[i] = nums.length;
+            } else {
+                nge[i] = st.peek();
             }
             
-            q.addLast(i);
-            
-            if(i >= k - 1){
-                res[idx++] = nums[q.getFirst()];
-           }
+            st.push(i);
         }
         
+        
+        int[] res = new int[nums.length - k + 1];
+        int j = 0;
+        for(int i = 0; i < res.length; i++){
+            if(j < i){
+                j = i;
+            }
+            
+            while(nge[j] <= i + k - 1){
+                j = nge[j];
+            }
+            
+            res[i] = nums[j];
+        }
+     
         return res;
     }
 }
