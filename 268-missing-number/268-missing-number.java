eacb5396cf77274da3f68327883aa99bf65ae020
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-08-31 14:03:11 +0530

    Time: 1 ms (78.38%), Space: 51.3 MB (34.01%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public static int arraySum(int[] arr2) {
+        // int sum = Integer.MAX_VALUE;
+        int sum = 0;
+        for(int i = 0; i< arr2.length; i++) {
+            sum += arr2[i];
+        }
+        
+        return sum;
+    }
+    
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int s = n * (n + 1)/2;
+        
+        int m = s - arraySum(nums);
+        
+        return m;
+    }
+}
