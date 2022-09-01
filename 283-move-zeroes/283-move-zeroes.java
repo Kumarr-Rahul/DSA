39334fe1cbe6661d92cb773975c02ccaf057d397
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-09-01 21:58:41 +0530

    Time: 75 ms (8.08%), Space: 55.3 MB (9.82%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -5,7 +5,7 @@ class Solution {
             if(nums[i] == 0){
                 
                 int idx = i;
-                while(idx < nums.length - 1) {
+                while(idx < nums.length - 1 && nums[idx + 1] != 0) {
                     int temp = nums[idx];
                     nums[idx] = nums[idx + 1];
                     nums[idx + 1] = temp;
