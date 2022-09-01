feb59473d0d549fb7b33e80b625ea3527c65da80
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-09-01 22:01:36 +0530

    Time: 58 ms (9.93%), Space: 43.9 MB (90.54%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -5,7 +5,7 @@ class Solution {
             if(nums[i] == 0){
                 
                 int idx = i;
-                while(idx < nums.length - 1 && nums[idx + 1] != 0) {
+                while(idx < nums.length - 1 && nums[idx + 1] != 0) { //nums[idx + 1] is not necessary, this                                                                         avoid swap when last element is zero
                     int temp = nums[idx];
                     nums[idx] = nums[idx + 1];
                     nums[idx + 1] = temp;
