21c304dd47f3665f44bb458ba35944d4f348fcca
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-09-04 23:35:26 +0530

    Time: 239 ms (14.28%), Space: 134.6 MB (9.53%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,35 +1,13 @@
-// class Solution {
-//     public int[] dailyTemperatures(int[] temp) {
-//         int[] ans = new int[temp.length];
-//         Stack<Integer> st = new Stack<>();
-//         st.push(0);
-        
-//         for(int i = 1; i < temp.length; i++){
-//             while(st.size() > 0 && temp[st.peek()] < temp[i]){
-//                 ans[st.peek()] = i - st.peek();
-//                 st.pop();
-//             }
-            
-//             st.push(i);
-//         }
-        
-//         return ans;
-//     }
-// }
-
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
         int[] ans = new int[temp.length];
         Stack<Integer> st = new Stack<>();
-        st.push(temp.length - 1);
+        st.push(0);
         
-        for(int i = temp.length - 2; i >= 0; i--){
-            while(st.size() > 0 && temp[st.peek()] <= temp[i]){
+        for(int i = 1; i < temp.length; i++){
+            while(st.size() > 0 && temp[st.peek()] < temp[i]){
+                ans[st.peek()] = i - st.peek();
                 st.pop();
-            }
-            
-            if(st.size() > 0){
-                ans[i] = st.peek() - i;
             }
             
             st.push(i);
@@ -38,3 +16,25 @@ class Solution {
         return ans;
     }
 }
+
+// class Solution {
+//     public int[] dailyTemperatures(int[] temp) {
+//         int[] ans = new int[temp.length];
+//         Stack<Integer> st = new Stack<>();
+//         st.push(temp.length - 1);
+        
+//         for(int i = temp.length - 2; i >= 0; i--){
+//             while(st.size() > 0 && temp[st.peek()] <= temp[i]){
+//                 st.pop();
+//             }
+            
+//             if(st.size() > 0){
+//                 ans[i] = st.peek() - i;
+//             }
+            
+//             st.push(i);
+//         }
+        
+//         return ans;
+//     }
+// }
