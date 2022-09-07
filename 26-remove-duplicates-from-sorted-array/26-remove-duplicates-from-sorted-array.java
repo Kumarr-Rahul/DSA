03f6510484b3e9a6af6be9e48896889a51fc1249
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-09-07 18:19:34 +0530

    Time: 2 ms (42.35%), Space: 47.4 MB (76.69%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,51 +1,35 @@
-// class Solution {
-//     public int removeDuplicates(int[] nums) {
-//         int n = nums.length;
-        
-//         int a = 0;
-//         int b = 1;
-//         int ans = 0;
-        
-//         while(b < n) {
-            
-//             if(nums[a] != nums[b]) {
-//                 a++;
-//                 //swap
-//                 int temp = nums[a];
-//                 nums[a] = nums[b];
-//                 nums[b] = temp;
-                
-//                 b++;
-                
-//             }else {
-//                 b++;
-//             }
-            
-//         }
-        
-//         ans = a + 1;
-        
-//         //not required
-//         // for(int i = ans; i < n; i++) {
-//         //     nums[i] = '_';
-//         // }
-        
-//         return ans;
-//     }
-// }
-
-//Approach 2
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n = nums.length;
-        if (n < 2)
-            return n;
-
-        int i = 1;
-        for (int j = i; j < n; j++)
-            if (nums[j] != nums[i - 1])
-                nums[i++] = nums[j];
-
-        return i;
+        
+        int a = 0;
+        int b = 1;
+        int ans = 0;
+        
+        while(b < n) {
+            
+            if(nums[a] != nums[b]) {
+                a++;
+                //swap
+                int temp = nums[a];
+                nums[a] = nums[b];
+                nums[b] = temp;
+                
+                b++;
+                
+            }else {
+                b++;
+            }
+            
+        }
+        
+        ans = a + 1;
+        
+        //not required
+        // for(int i = ans; i < n; i++) {
+        //     nums[i] = '_';
+        // }
+        
+        return ans;
     }
 }
