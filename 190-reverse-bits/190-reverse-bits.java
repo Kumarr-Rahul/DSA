df5c9939218f26a29727770d563b59bffd4a88dd
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-09-08 17:30:07 +0530

    Time: 2 ms (29.42%), Space: 42.6 MB (46.28%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -1,13 +1,40 @@
+// public class Solution {
+//     // you need treat n as an unsigned value
+//     public int reverseBits(int n) {
+//         if (n == 0) return 0;
+//         int revrs = 0;
+//         for (int i = 0; i < 32; i++) {
+//             revrs <<= 1;
+//             if ((n & 1) == 1) revrs++;
+//             n >>= 1;
+//         }
+//         return revrs;
+//     }
+// }
+
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        if (n == 0) return 0;
-        int revrs = 0;
-        for (int i = 0; i < 32; i++) {
-            revrs <<= 1;
-            if ((n & 1) == 1) revrs++;
-            n >>= 1;
+        if(n == 0) return 0;
+        
+        boolean flag = false;
+        int rev = 0;
+        int j = 0;
+        
+        for(int i = 31; i >= 0; i--) {
+           int mask = (1 << i); //left shifting my i times
+            
+            if((n & mask) != 0) {
+                    int smask = (1 << j);
+                    rev |= smask; 
+                    j++;
+            } else {
+                j++; 
+            }
+                
+                 
         }
-        return revrs;
+        
+        return rev;
     }
 }
