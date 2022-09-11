7dd60a6150b06399bd3a96e239362043f89fb00f
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-09-11 17:39:28 +0530

    Time: 12 ms (8.85%), Space: 56.6 MB (61.33%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -1,28 +1,43 @@
+// class Solution {
+//     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+//         int count = 0;
+        
+//         for(int i = 0; i < items.size(); i++) {
+//             if(ruleKey.equals("type")) {
+//                 String type = items.get(i).get(0);
+//                 if(ruleValue.equals(type)) {
+//                     count++;
+//                 }
+                
+//             }else if(ruleKey.equals("color")) {
+//                 String color = items.get(i).get(1);
+//                 if(ruleValue.equals(color)) {
+//                     count++;
+//                 }
+                
+//             }else {
+//                 String name = items.get(i).get(2);
+//                 if(ruleValue.equals(name)) {
+//                     count++;
+//                 }
+//             }
+//         }
+        
+//         return count;
+//     }
+// }
+//Short but same complexity
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int count = 0;
+        int res = 0;
         
-        for(int i = 0; i < items.size(); i++) {
-            if(ruleKey.equals("type")) {
-                String type = items.get(i).get(0);
-                if(ruleValue.equals(type)) {
-                    count++;
-                }
-                
-            }else if(ruleKey.equals("color")) {
-                String color = items.get(i).get(1);
-                if(ruleValue.equals(color)) {
-                    count++;
-                }
-                
-            }else {
-                String name = items.get(i).get(2);
-                if(ruleValue.equals(name)) {
-                    count++;
-                }
-            }
+        for(int i = 0 ;i<items.size();i++){
+            if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) res++;
+            if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) res++;
+            if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)) res++;
         }
         
-        return count;
+        return res;
+        
     }
 }
