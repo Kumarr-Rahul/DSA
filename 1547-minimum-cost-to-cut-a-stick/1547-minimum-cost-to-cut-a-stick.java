416184d43add804e5210dd5b467a1ebb25fc06c9
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-09-27 23:52:59 +0530

    Time: 43 ms (47.44%), Space: 42.9 MB (58.74%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -11,20 +11,15 @@ class Solution {
         int[][] dp = new int[list.size()][list.size()];
         for(int g = 2; g < dp[0].length; g++){
             for(int i = 0, j = g; j < dp[0].length; i++, j++){
-                
                 dp[i][j] = Integer.MAX_VALUE;
+                
                 for(int k1 = 1, k2 = g - 1; k1 < g; k1++, k2--){
-                    int left = dp[i][j - k2];
-                    int right = dp[i + k1][j];
-                    int factor = list.get(j) - list.get(i);
-                    
-                    dp[i][j] = Math.min(dp[i][j], left + right + factor);
+                    int factor = dp[i][j - k2] + dp[i + k1][j] + list.get(j) - list.get(i);
+                    dp[i][j] = Math.min(dp[i][j], factor);
                 }
             }
         }
         
-        return dp[0][dp[0].length - 1];
+        return dp[0][list.size() - 1];
     }
-    
-   
 }
