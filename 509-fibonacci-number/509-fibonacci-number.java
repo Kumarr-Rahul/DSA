4c6e15271b4a9c364753d9aa981610e08f34f232
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-09-30 23:18:20 +0530

    Time: 16 ms (20.37%), Space: 40.5 MB (71.25%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public int fib(int n) {
         
-        if(n == 0)
-            return 0;
-        if(n == 1)
-            return 1;
-        
-        return fib(n-1) + fib(n-2);
+        // Base Case
+		if (n <= 1)
+			return n;
+
+		// Recursive call
+		return fib(n - 1) + fib(n - 2);
     }
 }
