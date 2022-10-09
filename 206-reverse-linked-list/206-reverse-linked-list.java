f83c1fe696a0f9ad3e49c5d682a5a82620fceab4
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-10-09 19:01:23 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (81.71%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -8,33 +8,58 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         left = head;
+//         helper(head);  
+//         return head;
+//     }
+    
+//     boolean work = true;
+//     ListNode left;
+//     void helper(ListNode right){
+//         if(right == null){
+//             return;
+//         }
+//         helper(right.next);
+        
+//         if(work){
+//             if(left == right || left.next == right){
+//                 work = false;
+//             } 
+            
+//             int temp = left.val;
+//             left.val = right.val;
+//             right.val = temp;
+            
+//             left = left.next;
+//         }
+        
+//     }
+    
+// }
+
+//Approach 2
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-        left = head;
-        helper(head);  
-        return head;
+        if(head == null){
+            return null;
+        }
+        
+        helper(head);
+        head.next = null;
+        return nhead;
     }
     
-    boolean work = true;
-    ListNode left;
-    void helper(ListNode right){
-        if(right == null){
+    ListNode nhead;
+    void helper(ListNode node){
+        if(node.next == null){
+            nhead = node;
             return;
         }
-        helper(right.next);
         
-        if(work){
-            if(left == right || left.next == right){
-                work = false;
-            } 
-            
-            int temp = left.val;
-            left.val = right.val;
-            right.val = temp;
-            
-            left = left.next;
-        }
-        
-    }
-    
+        helper(node.next);
+        node.next.next = node;
+    } 
 }
