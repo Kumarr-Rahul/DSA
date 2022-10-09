f0e3bb88fd8d7572cac6c30073da435e94c18fd5
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-10-09 19:09:59 +0530

    Time: 2 ms (99.41%), Space: 42.7 MB (85.85%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -8,29 +8,67 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+//         ListNode dummy = new ListNode(-1);
+//         ListNode head = dummy, tail = dummy;
+//         int carry = 0;
+        
+//         while(l1 != null || l2 != null || carry > 0){
+            
+//             int d1 = (l1 == null) ? 0 : l1.val;
+//             int d2 = (l2 == null) ? 0 : l2.val;
+            
+//             ListNode temp = new ListNode((d1 + d2 + carry) % 10);
+//             carry = (d1 + d2 + carry) / 10;
+            
+//             tail.next = temp;
+//             tail = temp;
+            
+//             if(l1 != null) l1 = l1.next;
+//             if(l2 != null) l2 = l2.next;
+//         }
+        
+//         return dummy.next;
+        
+//     }
+// }
+
+
+
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-        ListNode dummy = new ListNode(-1);
-        ListNode head = dummy, tail = dummy;
+        ListNode head = null;
+        ListNode prev = null;
         int carry = 0;
         
-        while(l1 != null || l2 != null || carry > 0){
+        while(l1 != null || l2 != null || carry != 0){
+            ListNode res = new ListNode();
             
-            int d1 = (l1 == null) ? 0 : l1.val;
-            int d2 = (l2 == null) ? 0 : l2.val;
+            res.val = carry;
+            if(l1 != null){
+                res.val += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                res.val += l2.val;
+                l2 = l2.next;
+            }
             
-            ListNode temp = new ListNode((d1 + d2 + carry) % 10);
-            carry = (d1 + d2 + carry) / 10;
+            carry = res.val / 10;
+            res.val = res.val % 10;
             
-            tail.next = temp;
-            tail = temp;
-            
-            if(l1 != null) l1 = l1.next;
-            if(l2 != null) l2 = l2.next;
+            if(head == null){
+                head = res;
+                prev = res;
+            } else {
+                prev.next = res;
+                prev = res;
+            }
         }
         
-        return dummy.next;
-        
+        return head;
     }
 }
