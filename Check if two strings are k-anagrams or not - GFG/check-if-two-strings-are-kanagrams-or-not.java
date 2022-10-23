4b86476096fcd8f5f0e9c9a56aa82fba4f936bad
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-10-23 23:23:15 +0530

    Added solution - LeetHub

---
+++ Check if two strings are k-anagrams or not - GFG/check-if-two-strings-are-kanagrams-or-not.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+class GFG {
+    public static void main(String[] args)
+    {   Scanner sc = new Scanner(System.in);
+    	int t=sc.nextInt();
+    	while(t-->0)
+    	{
+    		String str1=sc.next();
+    		String str2=sc.next();
+    		int k=sc.nextInt();
+            Solution ob = new Solution();
+    		if (ob.areKAnagrams(str1, str2, k) == true)
+    			System.out.println("1");
+    		else
+    			System.out.println("0");
+    	}
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution {
+    boolean areKAnagrams(String s1, String s2, int k) {
+        // code here
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        
+        int[] map = new int[26];
+        for(int i = 0; i < s1.length(); i++){
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(i);
+            map[ch1 - 'a']++;
+            map[ch2 - 'a']--;
+        }
+        
+        int count = 0;
+        for(int val: map){
+            if(val >= 0){
+                count += val;
+            }
+        }
+        
+        return count <= k;
+    }
+}
