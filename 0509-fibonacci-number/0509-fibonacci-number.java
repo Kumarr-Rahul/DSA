de5b9b77468ec1f5b590b73d817c6107db3be822
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-10-29 19:50:35 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (22.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,22 +1,44 @@
-class Solution {
-    public int fib(int n, int[] dp) {
+//memo
+// class Solution {
+//     public int fib(int n, int[] dp) {
         
         
-		if (n <= 1) return n;
+// 		if (n <= 1) return n;
         
-        if(dp[n] != -1) return dp[n];
+//         if(dp[n] != -1) return dp[n];
         
-        dp[n] = fib(n - 1,dp) + fib(n - 2,dp);
+//         dp[n] = fib(n - 1,dp) + fib(n - 2,dp);
         
-		return dp[n];
+// 		return dp[n];
 
-    }
+//     }
     
-    public int fib(int n) {
+//     public int fib(int n) {
      
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp, -1);
         
-        return fib(n, dp);
-    }    
+//         return fib(n, dp);
+//     }    
+// }
+
+
+//Tabulation
+class Solution {
+    public int fib(int n) {
+        
+        if(n == 0) return 0;
+        
+        int[] dp = new int[n+1];
+        
+		dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+
+    }   
 }
