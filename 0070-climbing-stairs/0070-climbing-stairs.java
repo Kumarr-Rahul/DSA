a3f480befde1d5b7b6512e6b007c10ada18786c3
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-10-29 20:47:24 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (99.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,17 +2,33 @@ class Solution {
     
     public int climbStairs(int n) {
         
+        //code same as fibonacci series only change in line 9 -> as dp[0] = 1 as 0 means 1 way
+//         int[] dp = new int[n+1];
         
-        int[] dp = new int[n+1];
+// 		dp[0] = 1;
+//         dp[1] = 1;
         
-		dp[0] = 1;
-        dp[1] = 1;
+//         for(int i = 2; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+        
+//         return dp[n];
+        
+        //Space optimized O(1)
+        
+        int prev1 = 1;
+        int prev2 = 1;
+        int curr = 2;
+        
+        
         
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            prev2 = prev1;
+            prev1 = curr;
+            curr = prev1 + prev2;
         }
         
-        return dp[n];
+        return prev1;
         
     }
     
