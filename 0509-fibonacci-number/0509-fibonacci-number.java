7e2a1b2a970f3a2e348fe66d6ec8e10f9c63cbe7
Rahul Kumar <85256403+Kumarr-Rahul@users.noreply.github.com>
2022-10-29 19:43:07 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (6.48%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,22 @@
 class Solution {
-    public int fib(int n) {
+    public int fib(int n, int[] dp) {
         
-        // Base Case
-		if (n <= 1)
-			return n;
+        
+		if (n <= 1) return n;
+        
+        if(dp[n] != -1) return dp[n];
+        
+        dp[n] = fib(n - 1,dp) + fib(n - 2,dp);
+        
+		return dp[n];
 
-		// Recursive call
-		return fib(n - 1) + fib(n - 2);
     }
+    
+    public int fib(int n) {
+     
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        
+        return fib(n, dp);
+    }    
 }
